40b432f98e568e144e470dfb80fa7c69df1ad335
Krzysztof Langner <klangner@gmail.com>
2014-09-25 21:12:57 +0200

    Working websockets

--- proxy/main.py
+++ proxy/main.py
@@ -1,5 +1,23 @@
 
+import httplib
+from websocket import create_connection
+
+WS_URL = "ws://localhost:9000/_ws/test"
+
+def run():
+    socket = create_connection(WS_URL)
+    conn = httplib.HTTPConnection("bluenotepad.com")
+    while True:
+        msg = socket.recv()
+        conn.request("GET",msg)
+        response = conn.getresponse()
+        headers = ""
+        for (k,v) in response.getheaders():
+            headers += k + ":" + v + "\n"
+        headers += "\n" 
+        body = response.read() 
+        socket.send(headers + body)
 
 
 if __name__ == "__main__":
-    print("Hello from proxy")
+    run()
