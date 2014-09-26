f21ef041eb116d742b65df6c2136347034ead3f2
Krzysztof Langner <klangner@gmail.com>
2014-09-26 17:10:25 +0200

    Added msgId

--- proxy/main.py
+++ proxy/main.py
@@ -1,35 +1,59 @@
 import time
 import httplib
+import datetime
 from websocket import create_connection
 
-WS_URL = "ws://localhost:9000/_ws/test"
+# Where to connect with websockets
+TUNNELING_SERVER = "localhost:9000"
+# Unique for tunnel server device name (no spaces allowed)
+DEVICE_NAME = "test"
+# Provide content from this server
+LOCAL_SERVER_URL = "icplayer.com"
+
+
 
 def run():
-    socket = create_connection(WS_URL)
+    tunnelServerUrl = "ws://" + TUNNELING_SERVER + "/_ws/" + DEVICE_NAME
+    socket = create_connection(tunnelServerUrl)
     while True:
-        msg = socket.recv().decode("utf-8").split("\n")
-        conn = httplib.HTTPConnection("icplayer.com")
-        conn.request(msg[0], msg[1], msg[2])
-        response = conn.getresponse()
+        msg = socket.recv().decode("utf-8")
+        (msgId, method, path, body) = parseMessage(msg)
+        response = callWebserver(method, path, body)
         if response.status == 301:
-            conn = httplib.HTTPConnection("bluenotepad.com")
-            conn.request(msg[0], response.getheader("location", msg[1]), msg[2])
-            response = conn.getresponse()
-            
-        headers = str(response.status) + "\n"
+            response = callWebserver(method, response.getheader("location", path), body)
+        headers = buildHeaders(msgId, response)
+        body = response.read()
+        resp = bytes(headers) + body
+        socket.send_binary(resp)
+        
+        
+def parseMessage(msg):
+    ''' (msgId, method, path, body) '''
+    lines = msg.split("\n")
+    return (lines[0], lines[1], lines[2], lines[3])
+
+
+def buildHeaders(msgId, response):
+        headers = msgId + "\n" + str(response.status) + "\n"
         for (k,v) in response.getheaders():
             headers += k + ":" + v + "\n"
-        headers += "\n" 
-        body = response.read() 
-        resp = bytes(headers + body)
-        socket.send_binary(resp)
+        return headers + "\n"
+        
 
+def callWebserver(method, path, body):
+        conn = httplib.HTTPConnection(LOCAL_SERVER_URL)
+        conn.request(method, path, body)
+        return conn.getresponse()
 
 if __name__ == "__main__":
     while True:
+        run()
         try:
+            print(str(datetime.datetime.now()) + ": Starting connection")
             run()
         except Exception as e:
             print(type(e))
+            print(e)
+        print(str(datetime.datetime.now()) + ": Connection lost. Reconnecting in 5 seconds...")
         time.sleep( 5 )
     
